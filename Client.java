3f8fcca091c5ce8e1ae451579e766ea07ad2789f
Karl Kustav Muldia <karlkustav26@gmail.com>
2023-03-30 15:59:20 +0300

    Added clients.txt, tools.txt

--- Client.java
+++ Client.java
@@ -1,10 +1,9 @@
 import java.util.List;
 
 public class Client {
-    private final int nationalId;
-    private String firstName;
-    private String lastName;
-    private List<Tool> rentedTools;
+    private final int nationalId;   // Estonian national ID
+    private String firstName;       // First name
+    private String lastName;        // Last name
 
     public Client(int nationalId, String firstName, String lastName) {
         this.nationalId = nationalId;
@@ -13,7 +12,7 @@ public class Client {
     }
 
     public void addTool(Tool t) {
-        rentedTools.add(t);
+        int x;
     }
 
 }
--- Tool.java
+++ Tool.java
@@ -1,12 +1,14 @@
 import java.time.LocalDate;
 
 public class Tool {
-    private String category;
-    private String model;
-    private int productId;
-    private double price;
-    private boolean available;
-    private String returnDate;
+    private String category;        // Tool category
+    private String model;           // Tool model name
+    private final int productId;    // Product ID
+    private double price;           // Purchase price
+    private double rentalPrice;     // Rental price (7 days)
+    private boolean available;      // Availability
+    private String returnDate;      // Date to return tool
+    private int rentingClient;      // nationalId of client renting the tool
 
     public Tool(String category, String model, int productId, double price, boolean available) {
         this.category = category;
